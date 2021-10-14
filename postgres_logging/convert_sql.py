237c95e86b9bf4492a2c4a7fd6514381d109a38e
Kai Ze <36195611+Krazzen@users.noreply.github.com>
2021-10-14 11:41:37 +0800

    Update convert_sql.py

--- postgres_logging/convert_sql.py
+++ postgres_logging/convert_sql.py
@@ -4,15 +4,15 @@ import os
 
 list_of_files = glob.glob('/home/sadm/IFS/db_docker/logs/*') # * means all if need specific format then *.csv
 latest_file = max(list_of_files, key=os.path.getctime)
+list_of_files = ["logs/10-12_log0000.log","logs/10-13_log0000.log","logs/10-14_log0000.log"]
+sys.stdout = open("load_logs.data", "w")
+for name in list_of_files:
+    with open(name) as f:
 
-
-with open(latest_file) as f:
-    sys.stdout = open("load_user_address.sql", "w")
-    for line in f:
-        log_time = line.split('UTC')[0]
-        activity_done = line.split('UTC')[1]
-        # print("Log Time: " + log_time)
-        # print("Activity: " + activity_done)
-
-        print("CALL add_logs_data('" + log_time + "', '" + activity_done + "');")
-    sys.stdout.close()
+        for line in f:
+            if line is not None:
+                log_time = line.split('UTC')[0]
+                activity_done = line.split('UTC')[1]
+                print(log_time + ";" + activity_done)
+                #print("CALL add_logs_data('" + log_time + "', '" + activity_done + "');")
+sys.stdout.close()
